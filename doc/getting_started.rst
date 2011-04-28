196e97f79d0c72c5c689287af4d6a3a0404d3561
Francesco Banconi <frankban@gmail.com>
2011-04-28 18:15:50 +0200

    Model fixes.

---
+++ doc/getting_started.rst
@@ -0,0 +1,61 @@
+Getting started
+===============
+
+Requirements
+~~~~~~~~~~~~
+
+======  ======
+Python  >= 2.5
+Django  >= 1.0
+======  ======
+
+Installation
+~~~~~~~~~~~~
+
+The Mercurial repository of the application can be cloned with this command::
+
+    # TODO hg clone https://django-endless-pagination.googlecode.com/hg/ django-endless-pagination
+
+The ``ratings`` package, included in the distribution, should be
+placed on the ``PYTHONPATH``.
+
+Otherwise you can just ``pip install django-generic-ratings``.
+
+Settings
+~~~~~~~~
+
+Add the request context processor in your *settings.py*, e.g.::
+    
+    from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+    TEMPLATE_CONTEXT_PROCESSORS += (
+         'django.core.context_processors.request',
+    )
+    
+Add ``'ratings'`` to the ``INSTALLED_APPS`` in your *settings.py*.
+
+See :doc:`customization` section in this documentation for other settings options.
+
+Quickstart
+~~~~~~~~~~
+
+Having a template like this:
+
+.. code-block:: html+django
+
+    {% for object in objects %}
+        {# your code to show the entry #}
+    {% endfor %}
+    
+You can use Digg-style pagination to display objects just adding:
+
+.. code-block:: html+django
+
+    {% load endless %}
+    
+    {% paginate objects %}
+    {% for object in objects %}
+        {# your code to show the entry #}
+    {% endfor %}
+    {% show_pages %}
+    
+Done.
--- ratings/models.py
+++ ratings/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction, IntegrityError
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.utils.functional import memoize
@@ -14,7 +14,7 @@ class Score(models.Model):
     
     key = models.CharField(max_length=16)
     
-    average = models.IntegerField(default=0)
+    average = models.FloatField(default=0)
     total = models.IntegerField(default=0)
     num_votes = models.PositiveIntegerField(default=0)
         
@@ -63,21 +63,9 @@ def _get_content(instance_or_content):
     else:
         return ContentType.objects.get_for_model(instance_or_content), object_id
         
-_upsert_score_cache, _score_for_cache, _vote_for_cache = {}, {}, {}
-
-def upsert_score(instance_or_content, key):
-    """
-    Update or create current score values (average score, total score and 
-    number of votes) for target object *instance_or_content* and 
-    the given *key*. 
-    The argument *instance_or_content* can be a model instance or 
-    a sequence *(content_type, object_id)*.
-    """
-    content_type, object_id = _get_content(instance_or_content)
-    # now we have the content type and the object's pk
-    # TODO
+_get_score_for_cache, _get_vote_for_cache = {}, {}
    
-def score_for(instance_or_content, key):
+def get_score_for(instance_or_content, key):
     """
     Return the score instance for the target object *instance_or_content*
     and the given *key*.
@@ -91,7 +79,7 @@ def score_for(instance_or_content, key):
     except instance.ranking_scores.model.DoesNotExist:
         return None
         
-def vote_for(instance_or_content, key, user):
+def get_vote_for(instance_or_content, key, user):
     """
     Return the vote instance created by *user* for the target object 
     *instance_or_content* and the given *key*.
@@ -104,10 +92,66 @@ def vote_for(instance_or_content, key, user):
     except instance.ranking_scores.model.DoesNotExist:
         return None
 
-upsert_score = memoize(upsert_score, _upsert_score_cache, 2)
-score_for = memoize(score_for, _score_for_cache, 2)
-vote_for = memoize(vote_for, _vote_for_cache, 3)
+get_score_for = memoize(get_score_for, _get_score_for_cache, 2)
+get_vote_for = memoize(get_vote_for, _get_vote_for_cache, 3)
 
+def upsert_score(instance_or_content, key):
+    """
+    Update or create current score values (average score, total score and 
+    number of votes) for target object *instance_or_content* and 
+    the given *key*. 
+    The argument *instance_or_content* can be a model instance or 
+    a sequence *(content_type, object_id)*.
+    Return a sequence *score, created*.
+    """
+    content_type, object_id = _get_content(instance_or_content)
+    lookups = {
+        'content_type': content_type,
+        'object_id': object_id,
+        'key': key,
+    }
+    data = Vote.objects.filter(**lookups).aggregate(total=models.Sum('score'), 
+        average=models.Avg('score'), num_votes=models.Count('id'))
+    score, created = Score.objects.get_or_create(**lookups)
+    for k, v in data.items():
+        setattr(score, k, v)
+    score.save()
+    return score, created
+
+def upsert_vote(instance_or_content, key, score, **kwargs):
+    """
+    Update or create a vote instance described by *key*, *score*, other 
+    *kwargs* and *instance_or_content*, that can be a model instance or 
+    a sequence *(content_type, object_id)*.
+    Return a sequence *vote, created*.
+    """
+    content_type, object_id = _get_content(instance_or_content)
+    lookups = {
+        'content_type': content_type,
+        'object_id': object_id,
+        'key': key,
+    }
+    lookups.update(kwargs)
+    try:
+        vote = Vote.objects.get(**lookups)
+    except Vote.DoesNotExist:
+        try:
+            vote = Vote(**lookups)
+            vote.score = score
+            sid = transaction.savepoint(using=Vote.objects.db)
+            vote.save(force_insert=True, using=Vote.objects.db)
+            transaction.savepoint_commit(sid, using=Vote.objects.db)
+            return vote, True
+        except IntegrityError, e:
+            transaction.savepoint_rollback(sid, using=Vote.objects.db)
+            try:
+                vote = Vote.objects.get(**lookups)
+            except Vote.DoesNotExist:
+                raise e
+    vote.score = score
+    vote.save()
+    return vote, False
+    
 
 class RatedModel(models.Model):
     """
