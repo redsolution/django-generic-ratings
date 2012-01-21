06547f6f91549095f5fe2e38e60c2948a1aa4566
Francesco Banconi <frankban@gmail.com>
2012-01-21 12:52:06 +0100

    Some esthetic fixes.

--- ratings/forms/__init__.py
+++ ratings/forms/__init__.py
@@ -12,15 +12,15 @@ from widgets import SliderWidget, StarWidget
 class VoteForm(forms.Form):
     """
     Form class to handle voting of content objects.
-    
+
     You can customize the app giving a custom form class, following
     some rules:
-    
+
         - the form must define the *content_type* and *object_pk* fields
         - the form's *__init__* method must take as first and second positional
           arguments the target object getting voted and the ratings key
         - the form must define the *get_vote* method, getting the request and
-          a boolean *allow_anonymous* and returning an unsaved instance of 
+          a boolean *allow_anonymous* and returning an unsaved instance of
           the vote model
         - the form must define the *delete* method, getting the request and
           returning True if the form requests the deletion of the vote
@@ -32,10 +32,10 @@ class VoteForm(forms.Form):
         required=False)
     # security data
     timestamp = forms.IntegerField(widget=forms.HiddenInput)
-    security_hash = forms.CharField(min_length=40, max_length=40, 
+    security_hash = forms.CharField(min_length=40, max_length=40,
         widget=forms.HiddenInput)
     honeypot = forms.CharField(required=False, widget=forms.HiddenInput)
-    
+
     def __init__(self, target_object, key, score_range=None, score_step=None,
         can_delete_vote=None, data=None, initial=None):
         self.target_object = target_object
@@ -49,13 +49,13 @@ class VoteForm(forms.Form):
         super(VoteForm, self).__init__(data=data, initial=initial)
         self.fields['score'] = self.get_score_field(score_range, score_step,
             can_delete_vote)
-        
+
     # FACTORY METHODS
-    
+
     def get_score_field(self, score_range, score_step, can_delete_vote):
         """
         Return the score field.
-        Subclasses may ovveride this method in order to change 
+        Subclasses may ovveride this method in order to change
         the field used to store score value.
         """
         try:
@@ -66,17 +66,17 @@ class VoteForm(forms.Form):
             field = forms.FloatField if int(decimals) else forms.IntegerField
         widget = self.get_score_widget(score_range, score_step, can_delete_vote)
         return field(widget=widget, label=u'')
-            
+
     def get_score_widget(self, score_range, score_step, can_delete_vote):
         """
         Return the score widget.
-        Subclasses may ovveride this method in order to change 
+        Subclasses may ovveride this method in order to change
         the widget used to display score input.
         """
         return forms.TextInput
-        
+
     # SECURITY
-    
+
     def clean_security_hash(self):
         """
         Check the security hash.
@@ -88,7 +88,7 @@ class VoteForm(forms.Form):
             'timestamp' : self.data.get('timestamp', ''),
         }
         expected_hash = self.generate_security_hash(**security_hash_dict)
-        actual_hash = self.cleaned_data["security_hash"]
+        actual_hash = self.cleaned_data['security_hash']
         if not constant_time_compare(expected_hash, actual_hash):
             raise forms.ValidationError('Security hash check failed.')
         return actual_hash
@@ -101,12 +101,12 @@ class VoteForm(forms.Form):
         if time.time() - timestamp > (2 * 60 * 60):
             raise forms.ValidationError('Timestamp check failed')
         return timestamp
-        
+
     def clean_honeypot(self):
         """
         Check that nothing's been entered into the honeypot.
         """
-        value = self.cleaned_data["honeypot"]
+        value = self.cleaned_data['honeypot']
         if value:
             raise forms.ValidationError('Your vote is spam. Shame on you!')
         return value
@@ -142,15 +142,15 @@ class VoteForm(forms.Form):
         """
         Generate a HMAC security hash from the provided info.
         """
-        key_salt = "ratings.forms.VoteForm"
-        value = "-".join((content_type, object_pk, key, timestamp))
+        key_salt = 'ratings.forms.VoteForm'
+        value = '-'.join((content_type, object_pk, key, timestamp))
         return salted_hmac(key_salt, value).hexdigest()
-        
+
     # VOTE
-    
+
     def clean_score(self):
         """
-        If *score_range* was given to the form, then check if the 
+        If *score_range* was given to the form, then check if the
         score is in range.
         Again, if *score_step* was given, then check if the score is valid
         for that step.
@@ -181,34 +181,34 @@ class VoteForm(forms.Form):
             if int(score * factor) % int(self.score_step * factor):
                 raise forms.ValidationError('Score is not in steps')
         return score
-        
+
     def get_vote_model(self):
         """
         Return the vote model used to rate an object.
         """
         from ratings import models
         return models.Vote
-        
+
     def get_vote_data(self, request, allow_anonymous):
         """
-        Return two dicts of data to be used to look for a vote and to create 
-        a vote. 
-        
-        Subclasses in custom ratings apps that override *get_vote_model* can 
+        Return two dicts of data to be used to look for a vote and to create
+        a vote.
+
+        Subclasses in custom ratings apps that override *get_vote_model* can
         override this method too to add extra fields into a custom vote model.
-        
+
         If the first dict is None, then the lookup is not performed.
         """
         content_type = ContentType.objects.get_for_model(self.target_object)
-        ip_address = request.META.get("REMOTE_ADDR")
+        ip_address = request.META.get('REMOTE_ADDR')
         lookups = {
             'content_type': content_type,
             'object_id': self.target_object.pk,
-            'key': self.cleaned_data["key"],
+            'key': self.cleaned_data['key'],
         }
         data = lookups.copy()
         data.update({
-            'score': self.cleaned_data["score"],
+            'score': self.cleaned_data['score'],
             'ip_address': ip_address,
         })
         if allow_anonymous:
@@ -234,13 +234,13 @@ class VoteForm(forms.Form):
             # the voting process
             raise exceptions.DataError('Anonymous user cannot vote.')
         return lookups, data
-        
+
     def get_vote(self, request, allow_anonymous):
         """
-        Return an unsaved vote object based on the information in this form. 
+        Return an unsaved vote object based on the information in this form.
         Assumes that the form is already validated and will throw a
         ValueError if not.
-        
+
         The vote can be a brand new vote or a changed vote. If the vote is
         just created then the instance's id will be None.
         """
@@ -262,32 +262,32 @@ class VoteForm(forms.Form):
             vote.score = data['score']
             vote.ip_address = data['ip_address']
         return vote
-        
+
     # DELETE
-    
+
     def delete(self, request):
         """
         Return True if the form requests to delete the vote.
         """
         return self._delete_vote
-        
-        
+
+
 class SliderVoteForm(VoteForm):
     """
     Handle voting using a slider widget.
-    
+
     In order to use this form you must load the jQuery.ui slider
     javascript.
-    
+
     This form triggers the following javascript events:
-    
+
     - *slider_change* with the vote value as argument
       (fired when the user changes his vote)
     - *slider_delete* without arguments
       (fired when the user deletes his vote)
-      
+
     It's easy to bind these events using jQuery, e.g.::
-    
+
         $(document).bind('slider_change', function(event, value) {
             alert('New vote: ' + value);
         });
@@ -295,34 +295,34 @@ class SliderVoteForm(VoteForm):
     def get_score_widget(self, score_range, score_step, can_delete_vote):
         return SliderWidget(score_range[0], score_range[1], score_step,
             instance=self.target_object, can_delete_vote=can_delete_vote, key=self.key)
-        
-        
+
+
 class StarVoteForm(VoteForm):
     """
     Handle voting using a star widget.
-    
-    In order to use this form you must download the 
+
+    In order to use this form you must download the
     jQuery Star Rating Plugin available at
     http://www.fyneworks.com/jquery/star-rating/#tab-Download
     and then load the required javascripts and css, e.g.::
-    
+
         <link href="/path/to/jquery.rating.css" rel="stylesheet" type="text/css" />
         <script type="text/javascript" src="/path/to/jquery.MetaData.js"></script>
         <script type="text/javascript" src="/path/to/jquery.rating.js"></script>
-        
+
     This form triggers the following javascript events:
-    
+
     - *star_change* with the vote value as argument
       (fired when the user changes his vote)
     - *star_delete* without arguments
       (fired when the user deletes his vote)
-      
+
     It's easy to bind these events using jQuery, e.g.::
-    
+
         $(document).bind('star_change', function(event, value) {
             alert('New vote: ' + value);
         });
     """
     def get_score_widget(self, score_range, score_step, can_delete_vote):
-        return StarWidget(score_range[0], score_range[1], score_step, 
+        return StarWidget(score_range[0], score_range[1], score_step,
             instance=self.target_object, can_delete_vote=can_delete_vote, key=self.key)
--- ratings/forms/widgets.py
+++ ratings/forms/widgets.py
@@ -14,8 +14,8 @@ class BaseWidget(forms.TextInput):
     def get_parent_id(self, name, attrs):
         final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
         return final_attrs['id']
-        
-    def get_widget_id(self, prefix, name, key=""):
+
+    def get_widget_id(self, prefix, name, key=''):
         if self.instance:
             opts = self.instance._meta
             widget_id = '%s-%s-%s_%s-%s' % (prefix, name, opts.app_label, opts.module_name, self.instance.pk)
@@ -24,36 +24,36 @@ class BaseWidget(forms.TextInput):
         if key:
             widget_id = '%s_%s' % (widget_id, slugify(key))
         return widget_id
-            
+
     def get_values(self, min_value, max_value, step=1):
         decimal_step = Decimal(str(step))
         value = Decimal(str(min_value))
         while value <= max_value:
             yield value
-            value += decimal_step            
+            value += decimal_step
 
 class SliderWidget(BaseWidget):
     """
     Slider widget.
-    
+
     In order to use this widget you must load the jQuery.ui slider
     javascript.
-    
+
     This widget triggers the following javascript events:
-    
+
     - *slider_change* with the vote value as argument
       (fired when the user changes his vote)
     - *slider_delete* without arguments
       (fired when the user deletes his vote)
-      
+
     It's easy to bind these events using jQuery, e.g.::
-    
+
         $(document).bind('slider_change', function(event, value) {
             alert('New vote: ' + value);
         });
     """
     def __init__(self, min_value, max_value, step, instance=None,
-        can_delete_vote=True, key="", read_only=False, default='', 
+        can_delete_vote=True, key='', read_only=False, default='',
         template='ratings/slider_widget.html', attrs=None):
         """
         The argument *default* is used when the initial value is None.
@@ -68,12 +68,12 @@ class SliderWidget(BaseWidget):
         self.default = default
         self.template = template
         self.key = key
-    
+
     def get_context(self, name, value, attrs=None):
         # here we convert *min_value*, *max_value*, *step* and *value*
         # to string to avoid odd behaviours of Django localization
         # in the template (and, for backward compatibility we do not
-        # want to use the *unlocalize* filter) 
+        # want to use the *unlocalize* filter)
         attrs['type'] = 'hidden'
         return {
             'min_value': str(self.min_value),
@@ -90,40 +90,40 @@ class SliderWidget(BaseWidget):
             'label_id': 'slider-label-%s' % name,
             'remove_id': 'slider-remove-%s' % name,
         }
-        
+
     def render(self, name, value, attrs=None):
         context = self.get_context(name, value, attrs or {})
         return render_to_string(self.template, context)
-        
-    
+
+
 class StarWidget(BaseWidget):
     """
     Starrating widget.
-    
-    In order to use this widget you must download the 
+
+    In order to use this widget you must download the
     jQuery Star Rating Plugin available at
     http://www.fyneworks.com/jquery/star-rating/#tab-Download
     and then load the required javascripts and css, e.g.::
-    
+
         <link href="/path/to/jquery.rating.css" rel="stylesheet" type="text/css" />
         <script type="text/javascript" src="/path/to/jquery.MetaData.js"></script>
         <script type="text/javascript" src="/path/to/jquery.rating.js"></script>
-        
+
     This widget triggers the following javascript events:
-    
+
     - *star_change* with the vote value as argument
       (fired when the user changes his vote)
     - *star_delete* without arguments
       (fired when the user deletes his vote)
-      
+
     It's easy to bind these events using jQuery, e.g.::
-    
+
         $(document).bind('star_change', function(event, value) {
             alert('New vote: ' + value);
         });
     """
     def __init__(self, min_value, max_value, step, instance=None,
-        can_delete_vote=True, key="", read_only=False, 
+        can_delete_vote=True, key='', read_only=False,
         template='ratings/star_widget.html', attrs=None):
         super(StarWidget, self).__init__(attrs)
         self.min_value = min_value
@@ -134,7 +134,7 @@ class StarWidget(BaseWidget):
         self.read_only = read_only
         self.template = template
         self.key = key
-        
+
     def get_context(self, name, value, attrs=None):
         # here we convert *min_value*, *max_value* and *step*
         # to string to avoid odd behaviours of Django localization
@@ -147,7 +147,7 @@ class StarWidget(BaseWidget):
             split = u''
         else:
             values = self.get_values(self.min_value, self.max_value, self.step)
-            split = u' {split:%d}' % split_value            
+            split = u' {split:%d}' % split_value
         return {
             'min_value': str(self.min_value),
             'max_value': str(self.max_value),
@@ -166,7 +166,7 @@ class StarWidget(BaseWidget):
         if original:
             value = round(original * split) / split
             return Decimal(str(value))
-    
+
     def render(self, name, value, attrs=None):
         context = self.get_context(name, value, attrs or {})
         return render_to_string(self.template, context)
